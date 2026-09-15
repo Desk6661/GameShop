2b43f98cd46c0285ea277ef957d2499493bcfd77
desk6661 <sadhudip212@gmail.com>
2026-09-15 14:44:10 +0530

    stage 9 complete

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav>
+            <Link to="/">GameShop</Link>
+
+            <div>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
